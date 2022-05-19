4b43e09720aaf2142f37394f85e35014c21326a7
Abhay Soni <abhaysoni878@gmail.com>
2022-05-19 21:48:36 +0530

    push

---
+++ B.java
@@ -0,0 +1,16 @@
+public class A {
+	int add(int i,int j) {
+		return i+j;
+	}
+}
+public class B extends A{
+	
+	public static void main(String[] args) {
+		short s = 9;
+		System.out.println(add(s,6));
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
